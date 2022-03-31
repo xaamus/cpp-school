bfbd5f2a4786f8cdba10f15e1bf5e1a1b10e63fe
xaamus <93161575+xaamus@users.noreply.github.com>
2022-03-31 16:37:05 +0200

    Add files via upload

--- sortowanie.cpp
+++ sortowanie.cpp
@@ -1,82 +1,77 @@
-//NIE DZIALA
-
 #include <iostream>
 using namespace std;
-int n,j,i,op,sz;
-string wyb;
-int tab[0];
-void tablica(){
-    cout << "Ile elementow tablicy?" << endl;
-    cin >> n;
-    tab[n];
-    for (i=0;i<n;i++){
-        cin >> tab[i];
+int i,j,n,losfr,losto,losco,searchel,sortord,todo;
+string torn;
+int tab[100];
+void losowanieelementu(){
+    cout << "Podaj zakres losowania i ile elementow" << endl;
+    cin >> losfr;
+    cin >> losto;
+    cin >> losco;
+    for (i=0;i<losco;i++){
+        tab[i]=(rand()%(losto-losfr)+losfr);
     }
 }
-void szukanie(){
-    cout << "Podaj szukany element: " << endl;
-    cin >> sz;
-    for (i=0;i<n;i++){
-        if (tab[i]=i){
-            cout << "Znaleziono element";
-        }
-        else{
-            cout << "Nie znaleziono elementu";
+void szukanieelementu(){
+    cout << "Podaj szukany element" << endl;
+    cin >> searchel;
+    for (i=0;i<losco;i++){
+        if (searchel==tab[i]){
+            cout << "Element znajduje sie na " << i << "miejscu w tabeli" << endl;
         }
     }
 }
-void sortowaniemal(){
-    for(i=0;i<n;i++){
-		for(j=1;j<n-i;j++){
-		    if(tab[j-1]<tab[j]){
-			    swap(tab[j-1],tab[j]);
-		    }
-		}
-    }	
-}
-void sortowanieros(){
-    for(i=0;i<n;i++){
-		for(j=1;j<n-i;j++){
-		    if(tab[j-1]>tab[j]){
-			    swap(tab[j-1],tab[j]);
-		    }
-		}
+void sortowanie(){
+    cout << "Sortowanie rosnace czy malejace?" << endl;
+    cin >> sortord;
+    n=losco;
+    switch(sortord){
+        case 1:
+            for(i=0;i<n;i++){
+		        for(j=1;j<n-i;j++){
+		            if(tab[j-1]>tab[j]){
+			            swap(tab[j-1], tab[j]);
+		            }
+		        }
+            }
+            break;
+        case 2:
+            for(i=0;i<n;i++){
+		        for(j=1;j<n-i;j++){
+		            if(tab[j-1]<tab[j]){
+			            swap(tab[j-1], tab[j]);
+		            }
+		        }
+            }
+            break;
     }
-}
-void wyswietl(){
-    for (i=0;i<n;i++){
+    for (i=0;i<losco;i++){
         cout << tab[i] << endl;
     }
 }
 int main()
 {
-    do{
-        cout << "Co chcesz zrobic?" << endl;
-        cout << "1. Tworzenie tablicy" << endl;
-        cout << "2. Szukanie elementu" << endl;
-        cout << "3. Sortowanie malejaco" << endl;
-        cout << "4. Sortowanie rosnaco" << endl;
-        cin >> op;
-        switch(op){
+    do
+    {
+        cout << "Co chcesz zrobić?" << endl;
+        cout << "1. Losowanie" << endl;
+        cout << "2. Szukanie" << endl;
+        cout << "3. Sortowanie" << endl;
+        cin >> todo;
+        switch(todo){
             case 1:
-                tablica();
+                losowanieelementu();
                 break;
             case 2:
-                szukanie();
+                szukanieelementu();
                 break;
             case 3:
-                wyswietl();
-                sortowaniemal();
-                break;
-            case 4:
-                wyswietl();
-                sortowanieros();
+                sortowanie();
                 break;
         }
-        cout << "Powrot do menu" << endl;
-        cout << "tak / nie" << endl;
-        cin >> wyb;    
+        cout << "Czy chcesz wrocic do menu?" << endl;
+        cin >> torn;
     }
-    while(wyb=="tak");
+    while (torn=="tak");
     return 0;
 }
